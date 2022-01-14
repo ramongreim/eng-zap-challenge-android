1a0db8660929a27ef41db1586f628e82c525783a
Ramon Greim <ramon.greim@gmail.com>
2022-01-14 14:39:52 -0300

    Configuração inicial + Criação layout main activity - importação de ícones etc...
    (cherry picked from commit 2a9dac584d4a940d101b0eb25d339c1411d4f0f9)

---
+++ app/src/main/java/br/ramongreim/code_challenge_zap/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.ramongreim.code_challenge_zap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
